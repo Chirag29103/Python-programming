0fb13dc9fa20d929acf5177989bb33ef343923ba
Chirag29103 <122617858+Chirag29103@users.noreply.github.com>
2023-02-16 23:24:31 +0530

    Add files via upload

---
+++ Avg3.py
@@ -0,0 +1,6 @@
+''' To Find Average of Three Numbers'''
+x= int(input("Enter first Number:"))
+y= int(input("Enter second Number:"))
+z= int(input("Enter third Number:"))
+a=(x+y+z)/3
+print ("Average:",a)
